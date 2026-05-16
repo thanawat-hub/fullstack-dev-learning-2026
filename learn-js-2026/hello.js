7295fb46399e008fb883046fd95bd8b5c206dee1
Tor Thanawat <60775527+thanawat-hub@users.noreply.github.com>
2026-05-16 07:48:25 +0000

    Added(hello): เรียกใช้ js ผ่าน html

---
+++ learn-js-2026/hello.js
@@ -0,0 +1,16 @@
+alert('Hello File JS');
+// single line
+/* multi 
+line 
+of 
+comment */
+console.log('My name is Thanawat');
+
+console.log(1+2 * 3 /4); // สีม่วง คือ data types number
+
+console.log(1+'2'); // สีม่วง คือ data types string (การต่อข้อความ)
+
+console.log('2' * 3);
+
+console.log('I\'m Tor') //Escape slash
+console.log("Most people said \"T(h)or is a god of thunder not god of hammer\"")
