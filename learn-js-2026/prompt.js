21b3ac00466b37c0578f08c496af3838594d6714
Tor Thanawat <60775527+thanawat-hub@users.noreply.github.com>
2026-05-16 08:44:45 +0000

    Added(prompt): input text in JS

---
+++ learn-js-2026/prompt.js
@@ -0,0 +1,18 @@
+let boxx = null;
+console.log(typeof boxx); // This is Object มีค่าเป็นว่าง ไม่ใช่ไม่มีค่านะ
+
+// NaN -> 'NotANumber'
+
+const abc = prompt('Please input number');
+console.log(typeof abc); // This is string
+
+
+// convert typeI
+const a = +prompt('Please input number');
+console.log(typeof a); // This is number
+
+// convert typeII
+const b = +prompt('Please input number');
+console.log(typeof b); // This is number
+
+// console.log('parseInt', typeof)
