c459a47547e97f0f2e0498dccd9beec54af124b5
Tor Thanawat <60775527+thanawat-hub@users.noreply.github.com>
2026-06-13 06:44:12 +0000

    Edited(WelcomeMessage): refactor welcome component to concise arrow function

--- learn-js-2026/vite/my-app/src/components/WelcomeMessage.jsx
+++ learn-js-2026/vite/my-app/src/components/WelcomeMessage.jsx
@@ -9,14 +9,24 @@
 // }
 
 
-// ขอ function เดิม แต่เป็นรูปแบบของ arrow function
-const WelcomeMessage = () => {
-    return (   
-        // React Fragment คือการห่อหุ้มหลายๆ element โดยไม่ต้องเพิ่ม node ใหม่ใน DOM
-        <div> 
-            <h1>Welcome to My React App!</h1>
-            <p>This is a simple component (ชื่อ component ต้องเหมือนกับชื่อไฟล์)</p>
-        </div>
-    );
-}
+// // ขอ function เดิม แต่เป็นรูปแบบของ arrow function
+// const WelcomeMessage = () => {
+//     return (   
+//         // React Fragment คือการห่อหุ้มหลายๆ element โดยไม่ต้องเพิ่ม node ใหม่ใน DOM
+//         <div> 
+//             <h1>Welcome to My React App!</h1>
+//             <p>This is a simple component (ชื่อ component ต้องเหมือนกับชื่อไฟล์)</p>
+//         </div>
+//     );
+// }
+// export default WelcomeMessage;
+
+const WelcomeMessage = () => (
+  <div>
+    <div> 
+        <h1>Welcome to My React App!</h1>
+        <p>This is a simple component (ชื่อ component ต้องเหมือนกับชื่อไฟล์ [Best Practice])</p>
+    </div>
+  </div>
+)
 export default WelcomeMessage;
