597a83276beb8df06a0700c293a7262bef9e1881
Tor Thanawat <60775527+thanawat-hub@users.noreply.github.com>
2026-06-13 06:44:11 +0000

    Updated(App): update App layout to use learning components Users and Counter

--- learn-js-2026/vite/my-app/src/App.jsx
+++ learn-js-2026/vite/my-app/src/App.jsx
@@ -1,40 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Users from './components/Users'
 import { users } from './data/users.js'
-import { albums } from './data/albums.js'
-import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Card from './components/card.jsx'
-
+import Counter from './components/Counter'
 function App() {
-  // passing vairbles 
-  let message = 'Hello React!';
-  const cal = (a,b) => a + b;
-  const appStlye = {
-    color : 'red'
-  }
-  const profiles = [
-    { img: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp", title: "Shoes", description: "A card component has a figure, a body part, and inside body there are title and actions parts" },
-    { img: "src/assets/Screenshot.png", title: "Frontend Developer", description: "A card component has a figure, a body part, and inside body there are title and actions parts" },
-  ]
+  const appStyle = {
+    color: 'yellow'
+  };
+ 
   return (
-// ทำแบบนี้ไม่ได้ เนื่องจากกฏ JS มันต้องการ ให้ fn app มี 1 return -> ด้านหลัง DOM React จะแปลงโค้ด HTML เหล่านั้นให้กลายเป็นฟังก์ชัน JS
-    // <h1> Hello World</h1>
-    // <h1> Hello React</h1>
-    
-    <div style={ appStlye } className="txt">
-      <WelcomeMessage/>
-      <h1> This text is passing vairbles {message}</h1>
-      <p> Result of 5 + 3 is : {cal(5,3)}</p>
-      {
-        profiles.map((profile, index) => (
-          <Card img={profile.img} title={profile.title} description={profile.description}/>
-        ))
-      }
+    <div style={appStyle} className="mt-10">
+      <WelcomeMessage />
+      <Users/>
+      <Counter/>
     </div>
-  )
+  );
 }
-
 export default App
