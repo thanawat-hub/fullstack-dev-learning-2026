dd92441e7f06933ed0f684a065d5fc3a097861e3
Tor Thanawat <60775527+thanawat-hub@users.noreply.github.com>
2026-06-13 06:46:17 +0000

    Change(card_my): ทดลองเล่นเอง ไปใช้ตาม format อาจารย์เพื่อเรียนต่อ

---
+++ learn-js-2026/vite/my-app/src/App_custom.jsx
@@ -0,0 +1,41 @@
+// ใช้กับ card_custom.jsx
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+// import { users } from './data/users.js'
+// import { albums } from './data/albums.js'
+// import WelcomeMessage from './components/WelcomeMessage.jsx'
+// import Card from './components/Card.jsx'
+
+// function App() {
+//   // passing vairbles 
+//   let message = 'Hello React!';
+//   const cal = (a,b) => a + b;
+//   const appStlye = {
+//     color : 'red'
+//   }
+//   const profiles = [
+//     { img: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp", title: "Shoes", description: "A card component has a figure, a body part, and inside body there are title and actions parts" },
+//     { img: "src/assets/Screenshot.png", title: "Frontend Developer", description: "A card component has a figure, a body part, and inside body there are title and actions parts" },
+//   ]
+//   return (
+// // ทำแบบนี้ไม่ได้ เนื่องจากกฏ JS มันต้องการ ให้ fn app มี 1 return -> ด้านหลัง DOM React จะแปลงโค้ด HTML เหล่านั้นให้กลายเป็นฟังก์ชัน JS
+//     // <h1> Hello World</h1>
+//     // <h1> Hello React</h1>
+    
+//     <div style={ appStlye } className="txt">
+//       <WelcomeMessage/>
+//       <h1> This text is passing vairbles {message}</h1>
+//       <p> Result of 5 + 3 is : {cal(5,3)}</p>
+//       {
+//         profiles.map((profile, index) => (
+//           <Card img={profile.img} title={profile.title} description={profile.description}/>
+//         ))
+//       }
+//     </div>
+//   )
+// }
+
+// export default App
---
+++ learn-js-2026/vite/my-app/src/components/card_custom.jsx
@@ -0,0 +1,30 @@
+// import hero from '../assets/hero.png'
+
+// function Card({img="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp", title, description}, children) {
+//     console.log(img);
+//     console.log(title);
+//     console.log(description);
+//     const styleBody = {
+//         backgroundColor : 'red',
+//     }
+//     // import image ได้ 3 แบบ และอย่าตั้งชื่อซ้ำกัน
+//     // const img1 = 'src/assets/Screenshot.png'
+//     // const img2 = hero
+//     return (
+//         <div className="card bg-base-100 w-96 shadow-sm">
+//         <div className="card-body">
+//             <h2 className="card-title">{title}</h2>
+//             <p>{description}</p>
+//             <div className="card-actions justify-end">
+//             <button className="btn btn-primary">Buy Now</button>
+//             <br />
+//             {/* {img1 ? <img src={img1} alt="Card Image2" /> : <p>No image available</p>} */}
+//             {/* {img2 ? <img src={img2} alt="Card Image3" /> : <p>No image available</p>} */}
+//             {img && <img src={img} alt={title} />}
+//             </div>
+//         </div>
+//         </div>
+//     );
+// }
+
+// export default Card;
