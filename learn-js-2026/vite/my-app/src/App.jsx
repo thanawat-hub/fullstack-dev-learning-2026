1705554e97b7dc98f95fa7bf1b9125d06179d8aa
Tor Thanawat <60775527+thanawat-hub@users.noreply.github.com>
2026-06-13 07:21:00 +0000

    feat(App): add Timer import and placeholder comment

--- learn-js-2026/vite/my-app/src/App.jsx
+++ learn-js-2026/vite/my-app/src/App.jsx
@@ -3,6 +3,9 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Users from './components/Users'
 import { users } from './data/users.js'
 import Counter from './components/Counter'
+
+import Timer from './components/time_app/Timer'
+
 function App() {
   const appStyle = {
     color: 'yellow'
@@ -13,6 +16,7 @@ function App() {
       <WelcomeMessage />
       <Users/>
       <Counter/>
+      {/* <Timer/> */}
     </div>
   );
 }
