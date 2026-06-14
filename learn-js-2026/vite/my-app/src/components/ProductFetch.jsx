274fe6be45b94f74523fd0de38119875e7e9c249
Tor Thanawat <60775527+thanawat-hub@users.noreply.github.com>
2026-06-14 07:58:14 +0000

    Refector(Search): Logic if เพิ่ม match count and filter following search

--- learn-js-2026/vite/my-app/src/components/ProductFetch.jsx
+++ learn-js-2026/vite/my-app/src/components/ProductFetch.jsx
@@ -2,55 +2,149 @@ import { useEffect, useState } from "react";
 import Card from "./Card"
 import Search from "./Search"
 
-export default function Effects() {
-    const [products, setProduct] = useState([]); // step 1 ประกาศค่า default เพราะถ้า ขอข้อมูล (API) แปลว่าต้องรอ request ก่อน
-    const [selectedProduct, setSelectedProduct] = useState([null]); // default ให้เป็น null
-    const [FileteredProducts, setFileteredProducts] = useState([null])
-    // สร้าง function ใหม่ ก็ไม่ใช้บรรทัดที่ onClick={() => setSelectedProduct(product)} 
-    // จะเป็น handleDetail แทน และไม่ต้องมีบรรทัดที่ 22
-    // const handleDetail = (product) => {
-    //     setSelectedProduct(product);
-    // };
+// export default function Effects() {
+//     const [products, setProduct] = useState([]); // step 1 ประกาศค่า default เพราะถ้า ขอข้อมูล (API) แปลว่าต้องรอ request ก่อน
+//     const [selectedProduct, setSelectedProduct] = useState([null]); // default ให้เป็น null
+//     const [FileteredProducts, setFileteredProducts] = useState([null])
+//     // สร้าง function ใหม่ ก็ไม่ใช้บรรทัดที่ onClick={() => setSelectedProduct(product)} 
+//     // จะเป็น handleDetail แทน และไม่ต้องมีบรรทัดที่ 22
+//     // const handleDetail = (product) => {
+//     //     setSelectedProduct(product);
+//     // };
 
-    // step 3 เลือกใช้ useEffect mode อะไร จาก data ที่มาจากการ fetch
-    useEffect(() => {
-        fetch('https://dummyjson.com/products')
-        .then(res => res.json())
-        .then(data => {
-            // console.log(data.products)
-            // step 4 setProduct เข้าไปที่ state เพื่อโชว์ที่ pre
-            setProduct(data.products)
-            setSelectedProduct(selectedProduct)
-        });
+//     // step 3 เลือกใช้ useEffect mode อะไร จาก data ที่มาจากการ fetch
+//     useEffect(() => {
+//         fetch('https://dummyjson.com/products')
+//         .then(res => res.json())
+//         .then(data => {
+//             // console.log(data.products)
+//             // step 4 setProduct เข้าไปที่ state เพื่อโชว์ที่ pre
+//             setProduct(data.products)
+//             setSelectedProduct(selectedProduct)
+//         });
         
-    }, []);
+//     }, []);
 
-    const onSearchHandler = (value) => {
-        console.log(value)
+//     const onSearchHandler = (value) => {
+//         console.log(value)
+//     }
+
+//     // step 1 simples debug infor ใน pre tag จากค่าที่เราประกาศ in step 1
+//     return (
+//         <div>
+//             <h1>Products</h1>
+//             {/* โจทย์ บ่ายคือ ทำ search product เรื่อง paren and children*/}
+//             <Search onSearchHandler={onSearchHandler} />
+
+//             {/* โจทย์ต่อไป ต่อบ่ายให้ กด detail แล้วแสดงผลที่ search bar ตรงนี้ */}
+//             <div className='border rounded-2xl shadow-md bg-white p-6 my-4 text-left'>
+//                 {/* <p className='text-gray-400'>Show product detail when select</p> */}
+//                 <Card
+//                     key={selectedProduct.id}
+//                     title={selectedProduct.title}
+//                     imgUrl={selectedProduct.thumbnail}
+//                     description={selectedProduct.description}>
+//                 </Card>
+//             </div>
+            
+//             <div className='grid grid-cols-2 gap-2'>
+//             {
+//                 // step 5 เป็นจาก product data ให้render products into card components
+//                 products.map(product => (
+//                     // console.log(product)
+//                     <Card
+//                         key={product.id}
+//                         title={product.title}
+//                         imgUrl={product.thumbnail}
+//                         description={product.description}>
+//                             <button
+//                                 onClick={() => setSelectedProduct(product)}
+//                                 className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold px-6 py-2 rounded-lg shadow">
+//                                 Detail
+//                             </button>
+//                     </Card>
+//                 ))
+//             }
+//             </div>
+            
+//             { /* 0. simple debug info */ }
+//             {/* <pre>
+//                 {JSON.stringify(products, null, 2)}
+//             </pre> */}
+//         </div>
+//     )
+// }
+
+
+// === refactor ที่โค้ดตามอาจารย์ ใส่เงื่อนไข if ตอนกด detail
+
+export default function Products() {
+    // 0. define state to store products
+    const [products, setProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const [matched, setMatched] = useState(null);
+    const className = {
+        panel: 'border rounded-2xl shadow-md bg-white p-6 my-4 text-left',
+        panelH3: 'text-2xl font-bold mb-4 text-black',
+        panelImg: 'w-full h-64 object-cover mb-4 rounded-lg',
+        panelDescription: 'text-gray-700 mb-4',
+        panelPlaceholder: 'text-gray-400'
     }
 
-    // step 1 simples debug infor ใน pre tag จากค่าที่เราประกาศ in step 1
+    const onSearch = (value) => {
+        const newFilter = products.filter(p => p.description.toLowerCase().includes(value.toLowerCase()))
+        setFilteredProducts(newFilter);
+        // setMatched(filteredProducts.length)
+    }
+
+    // 2. Loading data from API
+    useEffect(() => {
+        fetch('https://dummyjson.com/products')
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data.products);
+                setFilteredProducts(data.products)
+                setMatched(data.products.length)
+            });
+    }, [])
+    // when filtered change -> update Match lenge
+    useEffect(() => {
+        setMatched(filteredProducts.length)
+    }, [filteredProducts])
+
     return (
         <div>
             <h1>Products</h1>
-            {/* โจทย์ บ่ายคือ ทำ search product เรื่อง paren and children*/}
-            <Search onSearchHandler={onSearchHandler} />
-
-            {/* โจทย์ต่อไป ต่อบ่ายให้ กด detail แล้วแสดงผลที่ search bar ตรงนี้ */}
-            <div className='border rounded-2xl shadow-md bg-white p-6 my-4 text-left'>
-                {/* <p className='text-gray-400'>Show product detail when select</p> */}
-                <Card
-                    key={selectedProduct.id}
-                    title={selectedProduct.title}
-                    imgUrl={selectedProduct.thumbnail}
-                    description={selectedProduct.description}>
-                </Card>
+            <Search onSearch={onSearch} matched={matched} />
+            <div className={className.panel}>
+                {
+                    selectedProduct ? (
+                        <div>
+                            <h3 className={className.panelH3}>
+                                {selectedProduct.title}
+                            </h3>
+                            <img
+                                src={selectedProduct.images[0]}
+                                alt={selectedProduct.title}
+                                className={className.panelImg} />
+                            <p
+                                className={className.panelDescription}>
+                                    {selectedProduct.description}
+                            </p>
+                        </div>
+                    ) : (
+                        <p className={className.panelPlaceholder}>Show product detail when select</p>
+                    )
+                }
+                {/* {
+                    <pre className='text-gray-400'>{JSON.stringify(selectedProduct)}</pre>
+                } */}
             </div>
-            
             <div className='grid grid-cols-2 gap-2'>
             {
-                // step 5 เป็นจาก product data ให้render products into card components
-                products.map(product => (
+                // 4. render products into card components
+                filteredProducts.map(product => (
                     // console.log(product)
                     <Card
                         key={product.id}
@@ -58,8 +152,8 @@ export default function Effects() {
                         imgUrl={product.thumbnail}
                         description={product.description}>
                             <button
-                                onClick={() => setSelectedProduct(product)}
-                                className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold px-6 py-2 rounded-lg shadow">
+                                className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold px-6 py-2 rounded-lg shadow cursor-pointer"
+                                onClick={() => setSelectedProduct(product)}>
                                 Detail
                             </button>
                     </Card>
@@ -74,69 +168,3 @@ export default function Effects() {
         </div>
     )
 }
-
-
-// // === refactor ที่โค้ดตามอาจารย์ ใส่เงื่อนไข if ตอนกด detail
-
-// import { useEffect, useState } from "react";
-// import Card from "./Card";
-
-// export default function Products() {
-//     // 0. define state to store products
-//     const [products, setProducts] = useState([]);
-//     const [selectedProduct, setSelectedProduct] = useState([null]);
-
-//     const className = {
-//         panel: 'border rounded-2xl shadow-md bg-white p-6 my-4 text-left',
-//         panelH3: 'text-2xl font-bold mb-4 text-black',
-//         panelImg: 'w-full h-64 object-cover mb-4 rounded-lg',
-//         panelDescription: 'text-gray-700 mb-4',
-//         panelPlaceholder: 'text-gray-400'
-//     }
-
-//     // 2. Loading data from API
-//     useEffect(() => {
-//         fetch('https://dummyjson.com/products')
-//             .then(res => res.json())
-//             .then(data => {
-//                 console.log(data.products);
-//                 setProducts(data.products);
-//                 // setSelectedProduct(selectedProduct)
-//             });
-//     }, [])
-
-//     return (
-//         <div>
-//             <h1>Products</h1>
-//             <div className='border rounded-2xl shadow-md bg-white p-6 my-4 text-left'>
-//                 <p className='text-gray-400'>Show product detail when select</p>
-//             </div>
-//             <div className='grid grid-cols-2 gap-2'>
-//                 {
-//                     setSelectedProduct ? ( // if condition true:false
-//                         <div>
-//                             <h3 className={className.panelH3}>
-//                             </h3>
-//                             <img
-//                                 src={selectedProduct.images[0]}
-//                                 alt={selectedProduct.title}
-//                                 className={className.pennelImg} />
-//                                 <p
-//                                     className={className.panelDescription}>
-//                                         {selectedProduct.description}
-//                                 </p>
-//                                 </div>
-
-//                     ) : (
-//                         <p className={className.panelPlaceholder}>Show product detail</p>
-//                     )
-//                 }
-//             </div>
-            
-//             { /* 0. simple debug info */ }
-//             {/* <pre>
-//                 {JSON.stringify(products, null, 2)}
-//             </pre> */}
-//         </div>
-//     )
-// }
--- learn-js-2026/vite/my-app/src/components/Search.jsx
+++ learn-js-2026/vite/my-app/src/components/Search.jsx
@@ -1,21 +1,26 @@
+// shared search component
 import { useState } from "react"
 
-export default function Search({onSearchHandler}) {
-    const [inputValue, setInputValue] = useState(''); // string แล้วแต่จะกำหนด บางครั้งก็ array ,null ขึ้นกับ data ตัวนี้ไว้สำหรับให้เขียน search ได้
-
-    const onChangeHandler = (value) => {
-        setInputValue(value);
-        onSearchHandler(value);
+// shared search component
+export default function Search({onSearch, matched}) {
+    const [value, setValue] = useState('');
+    const changeHandler = (value) => {
+        setValue(value);
+        onSearch(value);
     }
-
     return (
-        <div className="bg-white p-4 rounded-lg shadow flex gap-2">
+        <div className="bg-white p-4 rounded-lg shadow flex flex-col gap-2">
             <input
                 type="text"
                 name="search"
+                // value={value}
+                // onChange={(e) => changeHandler(e.target.value)}
+                onChange={(e) => onSearch(e.target.value)}
                 className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                onChange={(e) => onChangeHandler(e.target.value)}
             />
+            <div className="align-start">
+                <p>Match: {matched} </p>
+            </div>
         </div>
     )
 }
