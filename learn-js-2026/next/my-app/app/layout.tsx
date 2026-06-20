98b4798d010abaa2677c43d9ef54c45cfa11aa41
Tor-Thanawat <bunysiththiphi@gmail.com>
2026-06-20 11:08:10 +0700

    Change(product,home-apge,share layout): ตั้งเป็น home page และสร้าง product route ใหม่ และมี layout ด้านนอกให้ใช้กับทุกหน้าเว็บไซต์

--- learn-js-2026/next/my-app/app/layout.tsx
+++ learn-js-2026/next/my-app/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import Navbar from "../_components/Navbar"
+import Navbar from "./_components/Navbar"
 import { Geist, Geist_Mono } from "next/font/google";
 // import "./globals.css";
 
--- learn-js-2026/next/my-app/app/products/page.tsx
+++ learn-js-2026/next/my-app/app/products/page.tsx
@@ -4,19 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-    <h1> Welcome to Home Page </h1>
-    {/* <img src="/images/cover.png" */}
-    <Image //นิยใช้ tag นี้มากว่า img เพราะรองรับ Lazy loading 
-      // ไฟล์ใน public/ → เข้าถึงได้ผ่าน / โดยตรง ไม่ต้องใส่ root path
-      src="/danmachi.jpg" // มี 3 แบบw webp, png, jpg
-      alt="Danmachi Cover"
-      fill  // เติมให้เต็ม container
-      // or fixed size
-      // width={3840}
-      // height="2160"
-      className="object-cover"
-      priority
-      />
+    <h1> Welcome to Product Page </h1>
     </div>
   );
 }
