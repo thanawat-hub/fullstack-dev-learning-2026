b94cd8b22d24183d7d72396ed81799bfe883946a
Tor-Thanawat <bunysiththiphi@gmail.com>
2026-06-20 16:01:02 +0700

    Added(param[id]): เพิ่ม query และแสดงผลตาม id ใน artists

---
+++ learn-js-2026/next/my-app/app/artists/[id]/page.jsx
@@ -0,0 +1,30 @@
+// this is a: path param (of artist)
+import supabase from "@/app/libs/supabase"
+import Artists from "../page";
+// import Image from "next/image"
+// // เนื่องจาก next มีความปลอดภัย ต้องมีการ set permission allow fetch image ด้านนอก ตอนนี้ใช้ img ก่อน
+
+export default async function Artist( {params} ) {
+    const p = await params;
+    // console.log(p) // .id เพราะโฟลเดอร์ชื่อ [id] → param key คือ id
+    
+    const {data, error} = await supabase
+    .from('artists')
+    .select('')
+    .eq('id', p.id)
+    .limit(1)
+    .single() // ใช้แทนกับ limit ได้
+
+    const artist = data;
+    console.log(artist)
+    return (
+        <div>
+            {/* Hello This param is {p.id}  */}
+            <img
+                className="w-48"
+                src={artist.image}
+                alt={artist.name}
+            />
+        </div>
+    )
+}
--- learn-js-2026/next/my-app/app/artists/page.jsx
+++ learn-js-2026/next/my-app/app/artists/page.jsx
@@ -1,14 +1,39 @@
 "use server";
 import supabase from "@/app/libs/supabase"
+import Card from "@/app/components/Card"
+import Link from "next/link"
 
 export default async function Artists () {
     let artists = []
     const {data} = await supabase.from('songs').select('*')
-    console.log(data)
     artists = [...data];
     return (
-    <pre>
-        {JSON.stringify(data, null, 2)}
-    </pre>
+    // <pre>
+    //     {JSON.stringify(data, null, 2)}
+    // </pre>
+        <div>
+            <h1 className="text-3xl font-bold my-4 text-center"> Artists </h1>
+            <div className="grid grid-cols-1">
+                {
+                    // console.log(data)
+                    artists.map(data => (
+                        <Card
+                            key={data.id}
+                            title={data.title}
+                            imgUrl={data.cover_image}
+                            description={`Track ${data.track_number} • ${data.duration_seconds}s`}
+                        >
+                            {/* children — ปุ่มกดไป /artists/[id] */}
+                            <Link href={`/artists/${data.id}`}>
+                                <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold px-6 py-2 rounded-lg shadow">
+                                    ดูรายละเอียด
+                                </button>
+                            </Link>
+                        </Card>
+                    ))
+                }
+            </div>
+
+        </div>
     )
 }
