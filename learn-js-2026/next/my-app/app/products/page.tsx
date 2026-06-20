961a2f670ff76e5104813b07110c31dfd5b73d44
Tor-Thanawat <bunysiththiphi@gmail.com>
2026-06-20 10:55:57 +0700

    Added(page): เพิ่ม tag Imgae for lazzy loading

--- learn-js-2026/next/my-app/app/products/page.tsx
+++ learn-js-2026/next/my-app/app/products/page.tsx
@@ -1,7 +1,22 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <h1> Products </h1>
+    <div>
+    <h1> Welcome to Home Page </h1>
+    {/* <img src="/images/cover.png" */}
+    <Image //นิยใช้ tag นี้มากว่า img เพราะรองรับ Lazy loading 
+      // ไฟล์ใน public/ → เข้าถึงได้ผ่าน / โดยตรง ไม่ต้องใส่ root path
+      src="/danmachi.jpg" // มี 3 แบบw webp, png, jpg
+      alt="Danmachi Cover"
+      fill  // เติมให้เต็ม container
+      // or fixed size
+      // width={3840}
+      // height="2160"
+      className="object-cover"
+      priority
+      />
+    </div>
   );
 }
